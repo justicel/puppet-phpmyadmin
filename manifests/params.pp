5e6782ee1507f3d3f8a27a53c603aae2058ac31f
RD Admin user <radicaldesigns@radicaldesigns.org>
2013-02-20 12:13:00 -0800

    Fixes issues with templates to use configured values from phpmyadmin::params

--- manifests/params.pp
+++ manifests/params.pp
@@ -10,12 +10,14 @@ class phpmyadmin::params
       $apache_default_config = '/etc/httpd/conf.d/phpMyAdmin.conf'
       $config_file           = '/etc/phpMyAdmin/config.inc.php'
       $doc_path              = '/usr/share/phpMyAdmin'
+      $data_dir              = '/var/lib/phpMyAdmin'
     }
     /^(Debian|Ubuntu)$/: {
       $package_name = 'phpmyadmin'
       $apache_default_config = '/etc/phpmyadmin/apache.conf'
       $config_file           = '/etc/phpmyadmin/config.inc.php'
-      $doc_path              = '/usr/share/phpMyAdmin'
+      $doc_path              = '/usr/share/phpmyadmin'
+      $data_dir              = '/var/lib/phpmyadmin'
     }
   }
 
