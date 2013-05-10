6ed5c73b8567f845a5a470326846d55b5ab06bb0
Justice London <jlondon@syrussystems.com>
2013-05-10 12:53:06 -0700

    Some lint cleanup.

--- manifests/debconf.pp
+++ manifests/debconf.pp
@@ -6,7 +6,7 @@ class debconf
 define debconf::set_selection ( $selection = '', $value_type = 'string', $value = '' )
 {
   include debconf
-  exec{ "${name}":
+  exec{ $name:
     command => "/bin/echo debconf ${selection} ${value_type} ${value} | /usr/bin/debconf-set-selections",
     require => Package['debconf'],
     unless  => "/usr/bin/debconf-get-selections | /bin/egrep 'debconf[[:space:]]+${selection}+[[:space:]]+${value_type}+[[:space:]]+${value}'",
